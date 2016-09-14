1596e61ab5dab99e2e67928af51a7a28494bc5df
Peck <peck@localhost>
2016-09-14 18:47:00 +0200

    fix sh

--- compile.sh
+++ compile.sh
@@ -11,12 +11,12 @@ SP=src/java/
 
 echo "compiling nxt core..."
 find src/java/nxt/ -name "*.java" > sources.tmp
-javac -encoding utf8 -sourcepath "${SP}" -classpath "${CP}" -d classes/ @sources.tmp || exit 1
+javac -encoding utf8 -sourcepath "${SP}:src/evil/" -classpath "${CP}" -d classes/ @sources.tmp || exit 1
 echo "nxt core class files compiled successfully"
 
 echo "compiling nxt desktop..."
 find src/java/nxtdesktop/ -name "*.java" > sources.tmp
-javac -encoding utf8 -sourcepath "${SP}" -classpath "${CP}" -d classes/ @sources.tmp
+javac -encoding utf8 -sourcepath "${SP}:src/evil/" -classpath "${CP}" -d classes/ @sources.tmp
 if [ $? -eq 0 ]; then
     echo "nxt desktop class files compiled successfully"
 else
@@ -28,7 +28,9 @@ rm -f sources.tmp
 find addons/src/ -name "*.java" > addons.tmp
 if [ -s addons.tmp ]; then
     echo "compiling add-ons..."
-    javac -encoding utf8 -sourcepath "${SP}:addons/src" -classpath "${CP}:addons/classes:addons/lib/*" -d addons/classes @addons.tmp || exit 1
+    javac -encoding utf8 -sourcepath "${SP}:addons/src:src/evil/" -classpath "${CP}:addons/classes:addons/lib/*" -d 
+addons/classes 
+@addons.tmp || exit 1
     echo "add-ons compiled successfully"
     rm -f addons.tmp
 else
