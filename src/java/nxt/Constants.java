ee1f64545737f714b2f42a3060388346295da5c3
Peck <peck@localhost>
2016-09-16 16:11:57 +0200

    hard fork, new version required

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -102,7 +102,7 @@ public final class Constants {
     public static final int LAST_CHECKSUM_BLOCK = 0;
     public static final int LAST_KNOWN_BLOCK = isTestnet ? 0 : 0;
 
-    public static final int[] MIN_VERSION = Constants.isTestnet ? new int[] {0, 3} : new int[] {0, 3};
+    public static final int[] MIN_VERSION = Constants.isTestnet ? new int[] {0, 3, 2} : new int[] {0, 3, 2};
     public static final int[] MIN_PROXY_VERSION = new int[] {0, 3, 0};
 
     static final long UNCONFIRMED_POOL_DEPOSIT_NQT = (isTestnet ? 50 : 100) * ONE_NXT;
