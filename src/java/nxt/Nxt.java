737f0d0674457d1b605174ee5dea8018b09101c4
Peck <peck@localhost>
2016-09-19 17:07:25 +0200

    version bump

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -51,7 +51,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "0.3.4";
+    public static final String VERSION = "0.3.5";
     public static final String APPLICATION = "Elastic";
 
     private static volatile Time time = new Time.EpochTime();
