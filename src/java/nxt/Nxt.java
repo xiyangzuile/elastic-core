6a5dd7858184be0b3b5b19a989c8f40993add76b
Son of Man <SonOfMan@localhost.local>
2016-11-28 18:01:12 +0100

    MD5 branch

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -51,9 +51,10 @@ import nxt.util.ThreadPool;
 import nxt.util.Time;
 import nxt.util.Logger.Level;
 
+
 public final class Nxt {
 
-    public static final String VERSION = "0.7.2";
+    public static final String VERSION = "0.7.3";
     public static final String APPLICATION = "Elastic";
 
     private static volatile Time time = new Time.EpochTime();
