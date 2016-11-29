1b37dc6a83841a916a6756c98921ad4a2d28dc05
Son of Man <SonOfMan@localhost.local>
2016-11-29 09:24:39 +0100

    versio bump

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -108,8 +108,8 @@ public final class Constants {
     public static final int LAST_CHECKSUM_BLOCK = 0;
     public static final int LAST_KNOWN_BLOCK = isTestnet ? 0 : 0;
 
-    public static final int[] MIN_VERSION = Constants.isTestnet ? new int[] {0, 7, 2} : new int[] {0, 7, 2};
-    public static final int[] MIN_PROXY_VERSION = new int[] {0, 7, 2};
+    public static final int[] MIN_VERSION = Constants.isTestnet ? new int[] {0, 8, 0} : new int[] {0, 8, 0};
+    public static final int[] MIN_PROXY_VERSION = new int[] {0, 8, 0};
 
     static final long UNCONFIRMED_POOL_DEPOSIT_NQT = (isTestnet ? 50 : 100) * ONE_NXT;
 
--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -54,7 +54,7 @@ import nxt.util.Logger.Level;
 
 public final class Nxt {
 
-    public static final String VERSION = "0.7.3";
+    public static final String VERSION = "0.8.0";
     public static final String APPLICATION = "Elastic";
 
     private static volatile Time time = new Time.EpochTime();
