dae5352fa9957db1f8615c89f1d564bf00dbde67
Homosapien <homosapien@localhost>
2017-03-14 21:46:44 +0100

    No Economic-Clustering in SN broadcast

--- src/nxt/SupernodeMagicManager.java
+++ src/nxt/SupernodeMagicManager.java
@@ -27,20 +27,9 @@ public class SupernodeMagicManager {
     }
     public static void make(Attachment attachment, Appendix appdx, String secretPhrase, long recipientId, long amountNQT) throws Exception{
 
-        final int ecBlockHeight = 1;
-        long ecBlockId = 0;
 
-        if ((ecBlockId != 0) && (ecBlockId != Nxt.getBlockchain().getBlockIdAtHeight(ecBlockHeight))) {
-            throw new Exception("Failed setting ecBlock");
-        }
-        if ((ecBlockId == 0) && (ecBlockHeight > 0)) {
-            ecBlockId = Nxt.getBlockchain().getBlockIdAtHeight(ecBlockHeight);
-        }
 
         long feeNQT = 0;
-        if(attachment.getTransactionType().zeroFeeTransaction() == false){
-            // todo: set fees here
-        }
         short deadline = 1440;
 
         byte[] publicKey = null;
@@ -56,10 +45,6 @@ public class SupernodeMagicManager {
                 .referencedTransactionFullHash(null);
         if (attachment.getTransactionType().canHaveRecipient()) {
             builder.recipientId(recipientId);
-        }
-        if (ecBlockId != 0) {
-            builder.ecBlockId(ecBlockId);
-            builder.ecBlockHeight(ecBlockHeight);
         }
 
         if (appdx != null){
