0e712fd93127322a88c741179bd150371d24ff02
Baboon <you@example.com>
2017-03-30 17:54:09 +0200

    version bump

--- src/nxt/Nxt.java
+++ src/nxt/Nxt.java
@@ -194,7 +194,7 @@ public final class Nxt {
 
 	}
 
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 
 	public static final String APPLICATION = "Elastic";
 
