253df87e9bdd9f7ca35a9960de08bb87ce19bffe
Baboon <you@example.com>
2017-04-02 19:51:58 +0200

    tabula rasa script

--- tabula_rasa_testnet.sh
+++ tabula_rasa_testnet.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rm -rf elastic_test_db/ && git pull && mvn clean compile package && mvn exec:java
+rm -rf elastic_test_db/ && git pull && mvn clean compile package
+mvn exec:java 2> nxt_runtime_log.txt
