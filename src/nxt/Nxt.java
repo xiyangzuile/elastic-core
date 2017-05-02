92411b535446e06acc7eaf96d53ad8d9f2253d8f
Baboon <you@example.com>
2017-05-02 18:14:48 +0200

    experimental 1.12 setup

--- src/nxt/Nxt.java
+++ src/nxt/Nxt.java
@@ -201,7 +201,7 @@ public final class Nxt {
 
 	}
 
-	public static final String VERSION = "1.0.11";
+	public static final String VERSION = "1.0.12";
 
 	public static final String APPLICATION = "Elastic";
 
